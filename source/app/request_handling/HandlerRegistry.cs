a0b86053d49b052c0cc1fb77dbf1f742b80ed10b
Bryan Moore <bryan.moore@usablelife.com>
2014-10-08 12:52:37 -0400

    Pushing new changes

--- source/app/request_handling/HandlerRegistry.cs
+++ source/app/request_handling/HandlerRegistry.cs
@@ -1,10 +1,21 @@
-﻿namespace app.request_handling
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace app.request_handling
 {
   public class HandlerRegistry : IGetHandlersForRequests
   {
-    public IHandleOneRequest get_the_handler_that_can_handle(IProvideRequestDetails request)
-    {
-      throw new System.NotImplementedException();
-    }
+      IEnumerable<IHandleOneRequest> all_the_handlers;
+      public HandlerRegistry(IEnumerable<IHandleOneRequest> all_the_handlers)
+      {
+          this.all_the_handlers = all_the_handlers;
+      }
+
+      public IHandleOneRequest get_the_handler_that_can_handle(IProvideRequestDetails request)
+      {
+          var handler = all_the_handlers.Single(x => x.can_handle(request));
+          return handler;
+      }
   }
 }

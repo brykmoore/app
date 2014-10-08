020eaef331217c3e4e0ee5b704b9fe2f2f083ee2
Bryan Moore <bryan.moore@usablelife.com>
2014-10-07 21:39:52 -0400

    Pushing new changes

--- source/app/Calculator.cs
+++ source/app/Calculator.cs
@@ -1,11 +1,19 @@
 ﻿using System;
+using System.Data;
 
 namespace app
 {
   public class Calculator
   {
-    public int add(int first, int second)
+      IDbConnection connection;
+      public Calculator(IDbConnection connection)
+      {
+          this.connection = connection;
+      }
+
+      public int add(int first, int second)
     {
+          connection.Open();
       if (first < 0 || second < 0) throw new ArgumentException();
       return first + second;
     }

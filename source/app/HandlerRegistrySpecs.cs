d12b2299d78df1efa167edff3fc64492c05b0071
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2014-10-08 10:48:36 -0600

    Updated

--- source/app/HandlerRegistrySpecs.cs
+++ source/app/HandlerRegistrySpecs.cs
@@ -47,6 +47,34 @@ namespace app
         static List<IHandleOneRequest> all_the_handlers;
       }
         
+      public class and_it_does_not_have_the_handler
+      {
+        Establish c = () =>
+        {
+          request = fake.an<IProvideRequestDetails>();
+
+          all_the_handlers = Enumerable.Range(1, 100).Select(x => fake.an<IHandleOneRequest>()).ToList();
+          missing_handler = fake.an<IHandleOneRequest>();
+
+          depends.on<IEnumerable<IHandleOneRequest>>(all_the_handlers);
+          depends.on<ICreateTheMissingHandler>(x =>
+          {
+            x.ShouldEqual(request);
+            return missing_handler;
+          });
+        };
+
+        Because b = () =>
+          result = sut.get_the_handler_that_can_handle(request);
+
+        It returns_the_handler_created_by_the_missing_handler_builder = () =>
+          result.ShouldEqual(missing_handler);
+
+        static IHandleOneRequest result;
+        static IProvideRequestDetails request;
+        static List<IHandleOneRequest> all_the_handlers;
+        static IHandleOneRequest missing_handler;
+      }
     }
   }
 }

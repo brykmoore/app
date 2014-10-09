e82ef1ac6e86e391b4f2b6a1153be63ba0815f10
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2014-10-09 16:42:21 -0600

    Updated

--- source/app/startup/StartupPipelineBuilder.cs
+++ source/app/startup/StartupPipelineBuilder.cs
@@ -2,6 +2,9 @@
 {
   public class StartupPipelineBuilder : ICreateStartupPipelines
   {
+    public IRunAStartupStep initial_step;
+    public ICreateStartupSteps step_factory;
+
     public void finish_by<Step>() where Step : IRunAStartupStep
     {
       throw new System.NotImplementedException();

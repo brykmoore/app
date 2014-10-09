136e22d2aec0748a54be92a0fb51eb6c89e91c08
Bryan Moore <bryan.moore@usablelife.com>
2014-10-08 21:33:14 -0400

    Pushing new changes

--- source/app/containers.basic/Container.cs
+++ source/app/containers.basic/Container.cs
@@ -1,12 +1,23 @@
-﻿using app.containers.core;
+﻿using System.Web.Hosting;
+using app.containers.core;
 
 namespace app.containers.basic
 {
   public class Container : IGetDependencies
   {
-    public Dependency an<Dependency>()
-    {
-      throw new System.NotImplementedException();
-    }
+      IGetFactoriesForObjects factories;
+
+      public Container(IGetFactoriesForObjects factories)
+      {
+          this.factories = factories;
+      }
+
+      public Dependency an<Dependency>()
+      {
+          var factory =factories.get_factory_that_can_create(typeof(Dependency));
+          return (Dependency)factory.create();
+      }
+
+
   }
 }

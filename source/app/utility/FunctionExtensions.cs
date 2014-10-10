0aca5b8f4fa2991a4af8042aea6533e7571c6272
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2014-10-10 10:48:48 -0600

    Updated

--- source/app/utility/FunctionExtensions.cs
+++ source/app/utility/FunctionExtensions.cs
@@ -1,7 +1,12 @@
-﻿namespace app.utility
+﻿using System;
+
+namespace app.utility
 {
   public static class FunctionExtensions
   {
-     
+    public static Func<T, T> curry<T>(this Func<T, T, T> initial, T argument)
+    {
+      throw new NotImplementedException(); 
+    }
   }
 }
